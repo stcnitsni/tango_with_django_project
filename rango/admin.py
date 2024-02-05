2f204b02e721138f888d2346b5d6f095263572ae
Alexander Lee <2665964L@student.gla.ac.uk>
2024-02-05 17:24:24 +0000

    Chapter 5 (All tests complete)

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 # Register your models here.
+from rango.models import Category, Page
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,19 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# Create variable pointing towards static directory, should look like "Lab Work/tango_with_django_project/static"
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# Create variable pointing towards media directory
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+# Sets up media file hosting
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +68,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -117,5 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR,]
 
 STATIC_URL = '/static/'
